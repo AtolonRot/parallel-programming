5d3e2f51ca8ab5a92322f593e0ed356e2b2c8a58
Breyner Rojas <mon2bdr@gmail.com>
2020-10-03 15:57:04 -0500

    Distributed Week 3 finished

--- src/main/java/entry.java
+++ src/main/java/entry.java
@@ -6,13 +6,16 @@ public class entry {
 
     volatile int test;
     public static void main(String... args) {
-        System.out.println(calculateAcceleration(2.0, 0.39, 7.1));
-        isolated(() -> {
-
-        });
+        // System.out.println(calculateAcceleration(2.0, 0.39, 7.1));
+        System.out.println(velocidad(20.0, 2.0));
     }
 
     public static double calculateAcceleration(double x, double vI, double t) {
         return 2 * ((x - vI * t) / (t * t));
+    }
+
+    public static double velocidad(double angulo, double distancia) {
+        System.out.println("");
+        return Math.sqrt((distancia * 9.8) / Math.sin(angulo));
     }
 }
